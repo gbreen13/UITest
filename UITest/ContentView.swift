b668251964bfdf10722ae688feccb2b705ffe1e0
George Breen <george.breen@rabbithilsolutions.com>
2020-06-17 13:45:05 -0400

    Initial Commit

---
+++ UITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITest
+//
+//  Created by George Breen on 6/17/20.
+//  Copyright © 2020 George Breen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
